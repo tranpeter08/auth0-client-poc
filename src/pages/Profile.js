70736ff066319fce7d013fcce43d7e572dbf7a91
Peter Tran <tran.peter08@gmail.com>
2020-09-01 08:55:56 -0700

    created input for email issue w/ facebook login

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -6,46 +6,106 @@ import LogoutButton from '../components/LogoutButton';
 import {Redirect} from 'react-router-dom';
 
 export default function Profile(props) {
-  const {user, getAccessTokenSilently, getIdTokenClaims} = useAuth0();
+  const {
+    user,
+    getAccessTokenSilently,
+    getIdTokenClaims,
+    getAccessTokenWithPopup,
+  } = useAuth0();
   const [userMetadata, setUserMetadata] = useState(null);
   const [assets, setAssets] = useState(null);
+  const [email, setEmail] = useState('');
 
   useEffect(() => {
-    async function getUserInfo() {
-      try {
-        const [user_metadata, apiResp] = await Promise.all([
-          getUserMetaData(),
-          getAssests(),
-        ]);
-
-        setUserMetadata(user_metadata);
-        setAssets(apiResp);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
     if (user) {
       getUserInfo();
     }
   }, [user]);
 
+  async function getUserInfo() {
+    try {
+      const [user_metadata, apiResp] = await Promise.all([
+        getUserMetaData(),
+        getAssests(),
+      ]);
+
+      setUserMetadata(user_metadata);
+      setAssets(apiResp);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   async function getUserMetaData() {
-    const accessToken = await getAccessTokenSilently({
-      audience: `https://${auth0Config.domain}/api/v2/`,
-      scope: 'read:current_user',
-    });
+    try {
+      const accessToken = await getAccessTokenSilently({
+        audience: `https://${auth0Config.domain}/api/v2/`,
+        scope: auth0Config.scope,
+        redirect_uri: auth0Config.redirectUri,
+      });
 
-    const userDetailsByIdUrl = `https://${auth0Config.domain}/api/v2/users/${user.sub}`;
-    const metadataResponse = await fetch(userDetailsByIdUrl, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
-    });
+      const userDetailsByIdUrl = `https://${auth0Config.domain}/api/v2/users/${user.sub}`;
+      const metadataResponse = await fetch(userDetailsByIdUrl, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          'content-type': 'application/json',
+        },
+        method: 'get',
+      });
 
-    const {user_metadata} = await metadataResponse.json();
+      const data = await metadataResponse.json();
 
-    return user_metadata;
+      console.log(data);
+
+      return data.user_metadata;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function updateAuth0UserMetadata() {
+    try {
+      // const idToken = await getIdTokenClaims();
+
+      // console.log(idToken);
+      const userDetailsByIdUrl = `https://${auth0Config.domain}/api/v2/users/${user.sub}`;
+      const accessToken = await getAccessTokenSilently({
+        audience: `https://${auth0Config.domain}/api/v2/`,
+        scope: auth0Config.scope,
+        redirect_uri: auth0Config.redirectUri,
+      });
+
+      // const metadataResponse = await axios.patch(
+      //   RESOURCE_SERVER_URL + '/users/meta_data',
+      //   {user_metadata: 'trest'},
+      //   {
+      //     headers: {
+      //       Authorization: `Bearer ${idToken.__raw}`,
+      //       'Content-Type': 'application/json',
+      //     },
+      //   }
+      // );
+
+      // const data = await metadataResponse.json();
+      // console.log({data});
+
+      const resp = await axios.patch(
+        userDetailsByIdUrl,
+        {user_metadata: {email}},
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      console.log(resp.data);
+      setUserMetadata(resp.data.user_metadata);
+      // return data.user_metadata;
+    } catch (error) {
+      console.error(error.message);
+    }
   }
 
   async function getAssests() {
@@ -62,12 +122,34 @@ export default function Profile(props) {
     return res.data;
   }
 
+  // console.log({userMetadata, user});
+
   return (
     <div className="profile">
       <h1>PROFILE PAGE</h1>
       <img src={user.picture} alt={user.name} />
       <h2>Hello {user.name} !</h2>
-      <p>{user.email}</p>
+      <p>Email: {user.email || (userMetadata && userMetadata.email)}</p>
+      {user.email || (userMetadata && userMetadata.email) ? null : (
+        <div>
+          <label>
+            Please enter email address
+            <input
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </label>
+          <button
+            onClick={() => {
+              updateAuth0UserMetadata();
+            }}
+          >
+            SUBMIT
+          </button>
+        </div>
+      )}
       <h3>User Metadata</h3>
       {userMetadata ? (
         <pre>{JSON.stringify(userMetadata, null, 2)}</pre>
